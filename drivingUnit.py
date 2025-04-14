75fecd4f7fd1f206374fd045a6ad9ef3220f9994
Aldin M <aldinmemic52@gmail.com>
2025-04-14 18:03:10 +0200

    drivingUnit class added

---
+++ drivingUnit.py
@@ -0,0 +1,14 @@
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor
+from pybricks.parameters import Port
+from pybricks.robotics import DriveBase
+
+class DrivingUnit:
+    def __init__(self):
+        robot = DriveBase(Motor(Port.C), Motor(Port.D), wheel_diameter=55.5, axle_track=104)
+
+    
+    def startMoving(self):
+        robot.straight(1000)
+    
+    
